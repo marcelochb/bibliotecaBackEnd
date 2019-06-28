4f9bb8da18d1b88e1233712c56dd3ee4634aeeb2
Marcelo Block Teixeira <marcelochb@MacBook-Pro-de-Marcelo.local>
2019-06-28 01:42:04 -0300

    variavel de ambiente mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost/bibliotecaDB', { useMongoClient: true });
+mongoose.connect(process.env.MONGO_URL, { useMongoClient: true });
 //mongoose.connect('mongodb+srv://deploy:<password>@cluster0-seyff.mongodb.net/test?retryWrites=true&w=majority', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 //const connection = mongoose.connection;
