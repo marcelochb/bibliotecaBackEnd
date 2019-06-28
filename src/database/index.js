6aa3b050b1eef7c650a7b7d1698c3c04dc60a1f1
Marcelo Block Teixeira <marcelochb@MacBook-Pro-de-Marcelo.local>
2019-06-27 22:27:56 -0300

    ok

--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 
 mongoose.connect('mongodb://localhost/bibliotecaDB', { useMongoClient: true });
+//mongoose.connect('mongodb+srv://deploy:<password>@cluster0-seyff.mongodb.net/test?retryWrites=true&w=majority', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 //const connection = mongoose.connection;
 
